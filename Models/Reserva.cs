09fc7460dc7af2676df6d55eb75ad4d61c06540b
Wallace Oliveira <wall_h@hotmail.com>
2024-08-07 18:48:06 -0300

    Finalização do projeto DIO

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,17 +15,7 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
                 Hospedes = hospedes;
-            }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
         }
 
         public void CadastrarSuite(Suite suite)
@@ -35,26 +25,28 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
 
+            decimal valor = 0;
+            
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = DiasReservados * Suite.ValorDiaria;
+                valor = valor - (valor * 10 / 100);
+                return valor;
             }
+            else
+            {
+                valor = DiasReservados * Suite.ValorDiaria;
+                return valor;
 
-            return valor;
+            }
         }
     }
 }
